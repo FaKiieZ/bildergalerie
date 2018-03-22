3631497981d62ca99580e432a7ef5cd60812c02b
Remo Rothacher <rothacher.remo1@gmail.com>
2018-03-22 14:31:43 +0100

    gallery

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -69,7 +69,7 @@ require_once '../repository/UserRepository.php';
           }
           // Falls Login korrekt, Weiterleitung zum Feed
           else{
-              header("Location: " .$GLOBALS['appurl']);
+              header("Location: " .$GLOBALS['appurl'] .  "/gallery" );
               die();
           }
       }

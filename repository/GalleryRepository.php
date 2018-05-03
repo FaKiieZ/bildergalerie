ac99bf57a8e524a0c27a15abc9e060d4db9fbc3b
Remo Rothacher <rothacher.remo1@gmail.com>
2018-05-03 13:49:50 +0200

    bla

--- repository/GalleryRepository.php
+++ repository/GalleryRepository.php
@@ -6,12 +6,12 @@ require_once '../lib/Repository.php';
 
         protected $tableName = 'galerie';
 
-        public function create($kid, $gname ){
+        public function create($kid, $gname, $publiziert){
 
-            $query = "INSERT INTO $this->tableName (kid, gname, published) VALUES (?, ?, FALSE)";
+            $query = "INSERT INTO $this->tableName (kid, name, publiziert) VALUES (?, ?, ?)";
 
             $statement = ConnectionHandler::getConnection()->prepare($query);
-            $statement->bind_param('isi', $kid, $gname);
+            $statement->bind_param('isb', $kid, $gname, $publiziert);
 
             if (!$statement->execute()) {
                 throw new Exception ($statement->error);

6d1d4f9dfb949822a8794b4304bb5d30867043d9
Remo Rothacher <rothacher.remo1@gmail.com>
2018-05-17 14:25:52 +0200

    bla

--- view/gallery_view.php
+++ view/gallery_view.php
@@ -1,11 +1,10 @@
 <?php
 if (isset($data) && count($data) > 0){
-    echo '<div style="max-width: 90%;">';
     foreach ($data as $picture) {
-        echo '<img class="thumbnail col-md-4" style="object-fit: cover; width:230px; height:230px;" src="../var/www/uploads/' . $picture->name . '" />';
-        echo '<div class="col-sm-1"></div>';
+        echo '<div class="col-md-4">';
+        echo '<img class="thumbnail" style="object-fit: cover; width:230px; height:230px;" src="../var/www/uploads/' . $picture->name . '" />';
+        echo '</div>';
     }
-    echo '</div>';
 }else{
     echo "<h5>Keine Einträge vorhanden.</h5>";
 }
